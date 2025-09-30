f689c0dcf10eccdd1efb9ef9a026960c52fa57c3
David Giraldo <jedgiraldogo@unal.edu.co>
2025-09-29 22:13:42 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class Config:
         """
         Lee las variables de entorno en el momento de la instanciación.
         """
-        self.DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://user:password@mongo_db:27017/user_db")
+        self.DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://user:password@mongo_db:27017/comments_db?authSource=admin")
         self.SERVICE_PORT = int(os.getenv("SERVICE_PORT", 8000)) 
         self.LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
         self.SECRET_KEY = os.getenv("SECRET_KEY", "un_secreto")
